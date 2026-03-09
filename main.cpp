ae1ff74bb38b18b4397f71856906c3c50889bf34
SuberBAbdi <subaabdi4@gmail.com>
2026-03-09 18:24:41 +0000

    all functions

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include "mbed.h"
+
+DigitalOut Stepper_Motor_1_Speed(p5);
+DigitalOut Stepper_Motor_1_Direction(p6);
+DigitalOut Stepper_Motor_2_Speed(p7);
+DigitalOut Stepper_Motor_2_Direction(p8);
+
+// main() runs in its own thread in the OS
+int main()
+{
+    while (1) {
+             
+    }
+}
+
+void turn_left(){
+
+
+}
+
+void turn_right(){
+
+
+}
+
+void forwards(){
+
+}
+
+void backwards(){
+
+}
+
+void check_direction_choices(){
+
+}
+
+void flood_fill(){
+
+}
