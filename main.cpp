174b003f631497de9dd57e25ed5cb4ea4296fb70
SuberBAbdi <subaabdi4@gmail.com>
2026-03-11 16:37:26 +0000

    minor update_still planning in pseudocode

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
 #include "mbed.h"
-DigitalOut Stepper_Motor_1_Speed(p5);
+DigitalOut Stepper_Motor_1_Speed(p4);
 DigitalOut Stepper_Motor_1_Direction(p6);
 DigitalOut Stepper_Motor_2_Speed(p7);
 DigitalOut Stepper_Motor_2_Direction(p8);
+DigitalIn US_Sensor_Echo(p9);
 DigitalOut US_Sensor(p9);
-DigitalOut IR_Sensor_1(p10);
-DigitalOut IR_Sensor_2(p11);
-DigitalOut IR_Sensor_3(p12);
-DigitalOut IR_Sensor_4(p13);
-DigitalOut IR_Sensor_5(p14);
-DigitalOut IR_Sensor_6(p15);
-DigitalOut IR_Sensor_7(p16);
+AnalogIn IR_Sensor_1(p10);
+AnalogIn IR_Sensor_2(p11);
+AnalogIn IR_Sensor_3(p12);
+AnalogIn IR_Sensor_4(p13);
+AnalogIn IR_Sensor_5(p14);
+AnalogIn IR_Sensor_6(p15);
+AnalogIn IR_Sensor_7(p16);
 
-#define Max_Speed   20
-#define Base_Speed   (Max_Speed / 1)
-#define Turn_Speed  (Base_Speed)
-#define Reverse_Turn_Speed  (Turn_Speed - (Turn_Speed x 2))
-#define Delay 30ms
+
+
 
 int main(){
     int Last_Choice = 1;
