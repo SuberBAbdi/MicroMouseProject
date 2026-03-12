61d21d7ce1592a5be00b1b7cbdeb0be0f18c6ba8
SuberBAbdi <subaabdi4@gmail.com>
2026-03-12 16:24:06 +0000

    design changes need to be discussed now

--- main.cpp
+++ main.cpp
@@ -1,32 +1,60 @@
 #include "mbed.h"
-//busin best choice need to discuss combination
+//busin best choice need to discuss combination,       pins are temp change later
 DigitalOut Stepper_Motor_1_Speed(p5);
 DigitalOut Stepper_Motor_1_Direction(p6);
 DigitalOut Stepper_Motor_2_Speed(p7);
 DigitalOut Stepper_Motor_2_Direction(p8);
 DigitalIn US_Sensor_Echo(p9);
-DigitalOut US_Sensor_Trig(p9);
-BusIn IR_Sensors(p10,p11,p12,p13,p14,p15,p16);
+DigitalOut US_Sensor_Trig(p10);
+BusIn IR_Sensors(p11,p12,p13,p14,p15,p16,17);
 
-void check_US_sensor_distance(){}
+Timer t;
+void check_US_sensor_distance(    int Distance_from_Front_Wall;){
+    t.reset();
+    //add US distance code here
+    US_Sensor_Trig = 1;
+    wait_us(10000);
+    US_Sensor_Trig = 0;
+    t.start();
+    while (US_Sensor_Echo == 0){}
+    t.stop();
+    //add calcs here
+    if (Distance_from_Front_Wall <= 100){
+        pause_on_spot();
+
+    }
+}
+//ask about speed an whatnot on monday
 void forwards(){
     Stepper_Motor_1_Direction = 1;
     Stepper_Motor_2_Direction = 1;
 }
-void turn_left(){
+void turn_left(bool Last_Choice){
+    Last_Choice = true;
     Stepper_Motor_1_Direction = 0;
     Stepper_Motor_2_Direction = 1;
+    void
 }
 void turn_right(){
     Stepper_Motor_1_Direction = 1;
     Stepper_Motor_2_Direction = 0;
 }
-void backwards(){
+void backwards(bool Last_Choice){
+    Last_Choice = true;
     Stepper_Motor_1_Direction = 0;
     Stepper_Motor_2_Direction = 0;
 }
-void maze_location(){}
-void flood_fill(){
+void pause_on_spot(){
+    wait_us(10000);
+    check_movement();
+}
+void maze_location(bool Last_Choice){
+    if (Last_Choice == false){
+        int Distance_From_End = Distance_From_End - 1;
+    }
+    Last_Choice = false;
+}
+void check_movement(){
     switch(IR_Sensors) {
             case 0x1: forward(); break;
             case 0x2: backwards(); break;
@@ -39,13 +67,12 @@ int main(){
     bool Last_Choice = false;
     int No_of_Consecutive_Steps = 0;
     int Distance_from_End = 8;
+    int Distance_from_Front_Wall;
     while (1){
         if (Distance_from_End > 0) {
             check_US_sensor_distance();
-            flood_fill();
-            if (Last_Choice == false){
-                int Distance_From_End = Distance_From_End - 1;
-            }
+            check_movement();
+            maze_location();
         }
     }
 }
