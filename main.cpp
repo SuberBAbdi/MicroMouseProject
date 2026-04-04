c383565ff0bb8ff6912338e0c1c1c8642893bcee
SuberBAbdi <117910802+SuberBAbdi@users.noreply.github.com>
2026-04-04 01:26:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,80 +1,34 @@
 #include "mbed.h"
-#include "mbed2/299/drivers/LocalFileSystem.h"
 //Later change pins to right settings
-DigitalOut Stepper_Motor_1_Speed(p5);
+DigitalOut Stepper_Motor_1_Step(p5);
 DigitalOut Stepper_Motor_1_Direction(p6);
-DigitalOut Stepper_Motor_2_Speed(p7);
+DigitalOut Stepper_Motor_2_Step(p7);
 DigitalOut Stepper_Motor_2_Direction(p8);
 DigitalIn US_Sensor_Echo(p9);
 DigitalOut US_Sensor_Trig(p10);
-DigitalIn IR_SensorsL1(p11);
-DigitalIn IR_SensorsL2(p12);
-DigitalIn IR_SensorsL3(p13);
-DigitalIn IR_SensorsR1(p14);
-DigitalIn IR_SensorsR2(p15);
-DigitalIn IR_SensorsR3(p16);
-
+BusIn Left_Sensors (p11,p12,p13); // Just do busin instead
+BusIn Right_Sensors (p14,p15,p16);
 
 Timer t;
-void check_movement(){
-   //void check_movement(){
-    // Read all 6 IR sensors into a bitmask [L1,L2,L3,R1,R2,R3]
-   // uint8_t sensors = (IR_SensorsL1 << 5) |
-   //                   (IR_SensorsL2 << 4) |
-   //                   (IR_SensorsL3 << 3) |
-   //                   (IR_SensorsR1 << 2) |
-   //                   (IR_SensorsR2 << 1) |
-   //                   (IR_SensorsR3);
 
-    //bool left_wall  = (sensors >> 3) & 0x07;  // any of L1, L2, L3
-    //bool right_wall =  sensors       & 0x07;  // any of R1, R2, R3
-
-    //if (left_wall && !right_wall) {
-      //  turn_right();               // Too close to left wall
-    //} else if (!left_wall && right_wall) {
-      //  turn_left(Last_Choice);     // Too close to right wall
-    //} else {
-      //  forwards();                 // Centred — go straight
-    //}
-//}
-        t.start();
-        t.stop();
-    }
-//ask about speed an whatnot on monday
-void forwards(){
-    Stepper_Motor_1_Direction = 1;
-    Stepper_Motor_2_Direction = 1;
-}
-void turn_left(bool Last_Choice){  
-    Last_Choice = true;
-    Stepper_Motor_1_Direction = 0;
-    Stepper_Motor_2_Direction = 1;  
-}
-void turn_right(){
-    Stepper_Motor_1_Direction = 1;
-    Stepper_Motor_2_Direction = 0;
-}
-void backwards(bool Last_Choice){
-    Last_Choice = true;
-    Stepper_Motor_1_Direction = 0;
-    Stepper_Motor_2_Direction = 0;
-}
-void pause_on_spot(){
-    wait_us(10000);
-    check_movement();
-}
-void maze_location(bool Last_Choice,int Distance_From_End){
-    if (Last_Choice == false){
-        Distance_From_End = Distance_From_End - 1;
-    }
-    Last_Choice = false;
-}
-
+bool ECHO = false;
 volatile int us_distance_mm = 0;
 
-void check_us_distance(bool ECHO){
+// North = 0 ,East = 1, South = 2, West = 3
+int number_of_maze_walls[8][8];
+int mouse_position [8][8]; // location of the mouse
+int mouse_position_x = 0; // location of the mouse
+int mouse_position_y = 0; // location of the mouse
+int goal = 0; // lee numbers goal
+
+int number_of_maze_cells[8][8];
+int orientation = 0; // direction of the mouse, assume starting North always
+
+int first_calculate = 0;
+
+double check_us_distance(bool ECHO){
     uint32_t echo_time = 0; //supposed to have values range from 0 - 49000
-    
+
     //Trigger pulse function 
     US_Sensor_Trig = 0;
     wait_us(2);
@@ -88,32 +42,260 @@ void check_us_distance(bool ECHO){
 
     //Wait for echo low
     while(ECHO){
-    t.stop();
+        t.stop();
     }
     echo_time = t.read_us();
 
     //Speed of sound = 0.343 mm/us
     us_distance_mm = (echo_time*343)/2000;
     if(us_distance_mm < 80){
-    t.stop();
-}
-}
-
-
-int main(){
-    bool Last_Choice = false;
-    bool ECHO = false;
-    int No_of_Consecutive_Steps = 0;
-    int Distance_from_End = 8;
-    while (1){
-        if (Distance_from_End > 0) {
-            check_us_distance(true);
-            check_movement();
-            maze_location(Last_Choice, Distance_from_End);
+        t.stop();
         }
-        else {
-        t.reset();
+    return (double)us_distance_mm;
+    }
+
+int check_left_sensors(){
+    int left_wall_value = Left_Sensors.read();
+    if (left_wall_value == 7){
+        return 1;
+    }
+    // can added elseif for allignment issues but need hardware to test
+    else {
+        return 0;
+    }
+}
+
+int check_right_sensors(){
+    int right_wall_value = Right_Sensors.read();
+    if (right_wall_value == 7){
+        return 1;
+    }
+    // can added elseif for allignment issues but need hardware to test
+    else {
+        return 0;
+    }
+}
+
+bool wall_in_front(){
+    return check_us_distance(true) < 80;
+}
+bool wall_to_left(){
+    return check_left_sensors() == 1;
+}
+bool wall_to_right(){
+    return check_right_sensors() == 1;
+}
+
+void update_walls(int orientation, bool front, bool left, bool right){
+    if (orientation == 0){
+        if (front) number_of_maze_walls[mouse_position_x][mouse_position_y] |= 1; // Mark North Wall, Front is North (1)
+        if (left)  number_of_maze_walls[mouse_position_x][mouse_position_y] |= 8; // Mark West Wall, Left is West (8)
+        if (right) number_of_maze_walls[mouse_position_x][mouse_position_y] |= 2; // Mark East Wall, Right is East (2)
+        }
+    else if (orientation == 1){
+        if (front) number_of_maze_walls[mouse_position_x][mouse_position_y] |= 2; // Mark North Wall, Front is East (2)
+        if (left)  number_of_maze_walls[mouse_position_x][mouse_position_y] |= 1; // Mark West Wall, Left is North (1)
+        if (right) number_of_maze_walls[mouse_position_x][mouse_position_y] |= 4; // Mark East Wall, Right is is South (4)
+        }
+    else if (orientation == 2){
+        if (front) number_of_maze_walls[mouse_position_x][mouse_position_y] |= 4; // Mark North Wall, Front is South (4)
+        if (left)  number_of_maze_walls[mouse_position_x][mouse_position_y] |= 2; // Mark West Wall, Left is East (2)
+        if (right) number_of_maze_walls[mouse_position_x][mouse_position_y] |= 8; // Mark East Wall, Right is West (8)
+        }
+    else if (orientation == 3){
+        if (front) number_of_maze_walls[mouse_position_x][mouse_position_y] |= 8; // Mark North Wall, Front is West (8)
+        if (left)  number_of_maze_walls[mouse_position_x][mouse_position_y] |= 4; // Mark West Wall, Left is South (4)
+        if (right) number_of_maze_walls[mouse_position_x][mouse_position_y] |= 1; // Mark East Wall, Right is North (1)
+        }
+}
+
+void mouse_location( bool front, bool left, bool right){ // check line 137 error idk why
+    // North = 0
+    // East = 1
+    // South = 2
+    // West = 3
+
+    if (orientation == 0){
+        if(front == false){
+            mouse_position_y++;
+        }
+        else if (front == true && left == false) {
+            orientation = 3;
+            mouse_position_x--;
+        }
+        else if (front == true && left == true && right == false) {
+            orientation = 1;
+            mouse_position_x++;
+        }
+        else{
+            orientation = 2;
+            mouse_position_x++;
+        }
+    }
+
+    else if (orientation == 1){
+        if(front == false){
+            mouse_position_x++;
+        }
+        else if (front == true && left == false) {
+            orientation = 0;
+            mouse_position_y++;
+        }
+        else if (front == true && left == true && right == false) {
+            orientation = 2;
+            mouse_position_y--;
+        }
+        else{
+            orientation = 3;
+            mouse_position_x--;
+        }
+    }
+
+    else if (orientation == 2){
+        if(front == false){
+            mouse_position_y--;
+        }
+        else if (front == true && left == false) {
+            orientation = 1;
+            mouse_position_x++;
+        }
+        else if (front == true && left == true && right == false) {
+            orientation = 3;
+            mouse_position_x--;
+        }
+        else{
+            orientation = 0;
+            mouse_position_y++;
+        }
+    }
+
+    else if (orientation == 3){
+        if(front == false){
+            mouse_position_x--;
+        }
+        else if (front == true && left == false) {
+            orientation = 2;
+            mouse_position_y--;
+        }
+        else if (front == true && left == true && right == false) {
+            orientation = 0;
+            mouse_position_y++;
+        }
+        else{
+            orientation = 1;
+            mouse_position_x++;
         }
     }
 }
 
+void calculate_flood_fill(){
+    //lee's assign lee's number to eac
+}
+
+void flood_fill_setup(){ // need to fix, to setup up the 2d array details, of inital lees number
+    for (int y = 0; y < 8; y++) {
+        for (int x = 0; x < 8; x++) {
+            number_of_maze_cells [y][x] = (7 - x) + (7 - y) ;
+            }
+        }
+    calculate_flood_fill();
+}
+
+
+
+void flood_fill_update(){
+    // update lees number, and to control priority for t junctions
+}
+
+
+
+void steps(){ // expand on loops when testing hardware
+    Stepper_Motor_1_Step = 1; 
+    Stepper_Motor_2_Step = 1;
+    wait_us(1000);
+    Stepper_Motor_1_Step = 0; 
+    Stepper_Motor_2_Step = 0;
+    wait_us(1000);
+}
+
+void forwards(){
+    Stepper_Motor_1_Direction = 1;
+    Stepper_Motor_2_Direction = 1;
+    for (int i = 0; i < 400; i++){
+        steps();
+    }
+}
+void turn_left(){  
+    Stepper_Motor_1_Direction = 0;
+    Stepper_Motor_2_Direction = 1;
+    for (int i = 0; i < 400; i++){
+        steps();
+    }
+}
+
+void turn_right(){
+    Stepper_Motor_1_Direction = 1;
+    Stepper_Motor_2_Direction = 0;
+    for (int i = 0; i < 400; i++){
+        steps();
+    }
+}
+void u_turn(){
+    Stepper_Motor_1_Direction = 0;
+    Stepper_Motor_2_Direction = 0;
+    for (int i = 0; i < 400; i++){
+        steps();
+    }
+}
+
+int main(){
+   
+    void flood_fill_setup();
+
+    while (1){
+        wait_us(10000);
+        bool front = wall_in_front();
+        bool left = wall_to_left();
+        bool right = wall_to_right();
+        wait_us(10000);
+        void update_walls();
+        wait_us(10000);
+        int mouse_position = (mouse_position_x + mouse_position_y);
+        if ((mouse_position = goal)) {
+        // end program
+        }
+        //else if are priority based
+        else if(front == false){
+            wait_us(10000);
+            void mouse_location(); //update mouse_location
+            wait_us(10000);
+            forwards();
+            wait_us(10000);
+            flood_fill_update();
+            }
+        else if (front == true && left == false) {
+            wait_us(10000);
+            void mouse_location();
+            wait_us(10000);
+            turn_left();
+            wait_us(10000);
+            flood_fill_update();
+            }
+        else if (front == false && left == false && right == true) {
+            wait_us(10000);
+            void mouse_location();
+            wait_us(10000);
+            turn_right();
+            wait_us(10000);
+            flood_fill_update();
+            }
+        else if (front == true && left == true && right == true) { // uturn
+            wait_us(10000);
+            void mouse_location();
+            wait_us(10000);
+            u_turn();
+            wait_us(10000);
+            calculate_flood_fill(); // deadend, recalculate needs number
+        }
+    }
+}
